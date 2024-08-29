41d6abed0abbe0e99fa40bceb3ff4f7f4d7f913f
Santiago Aguirre <santiaguirre.lam@gmail.com>
2024-08-29 17:02:58 -0300

    Depurando codigo, cambios en urls.py / views.py

--- amt/products/admin.py
+++ amt/products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Marca, Categoria, SubCategoria, Producto, ImagenProducto,Atributos
+from .models import Marca, Categoria, SubCategoria, Producto, ImagenProducto
 
 # Register your models here.
 admin.site.register(Marca)
@@ -7,4 +7,3 @@ admin.site.register(Categoria)
 admin.site.register(SubCategoria)
 admin.site.register(Producto)
 admin.site.register(ImagenProducto)
-admin.site.register(Atributos)
--- amt/products/urls.py
+++ amt/products/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import por_categoria,por_subcategoria,test
+from .views import home
 
 app_name = "products"
 
 urlpatterns = [
-    path('categoria/<str:categoria_nombre>/', por_categoria, name='categoria_filter'),
-    path('categoria/<str:categoria_nombre>/<str:subcategoria_nombre>/', por_subcategoria, name='subcategoria_filter'),
-    path('test/',test,name='test')
+    path('categoria/', home)
 ]
