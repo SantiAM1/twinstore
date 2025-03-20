0b99ba27ef79d3cb7adcadee7bd8e96b43a8fc57
Santiago Aguirre <santiaguirre.lam@gmail.com>
2025-03-20 13:22:33 -0300

    finalizando estructuracion

--- amt/payment/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import HistorialCompras
-# Register your models here.
-admin.site.register(HistorialCompras)
--- amt/products/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from .models import Marca
-
-class FiltroProductoForm(forms.Form):
-    marca = forms.CharField(widget=forms.HiddenInput(), required=False)
-    conectividad = forms.CharField(widget=forms.HiddenInput(), required=False)
