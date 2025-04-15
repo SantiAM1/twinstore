165cc0e4ed1b894b05daf76dd2510a602163f880
Santiago Aguirre <santiaguirre.lam@gmail.com>
2025-04-15 16:32:46 -0300

    Creando modelo ModoMantenimiento

--- project/core/models.py
+++ project/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class ModoMantenimiento(models.Model):
+    activo = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "Sitio en mantenimiento" if self.activo else "Sitio activo"
+
+    class Meta:
+        verbose_name = "Modo Mantenimiento"
+        verbose_name_plural = "Modo Mantenimiento"
