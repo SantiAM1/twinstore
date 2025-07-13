34f1ea811b0ccc1de987752001266b6294b6817b
Santiago Aguirre <santiaguirre.lam@gmail.com>
2025-07-13 16:08:44 -0300

    Proteccion de re-ingreso manual a la url de 'subir el comprobante'

--- payment/views.py
+++ payment/views.py
@@ -86,10 +86,9 @@ def payment_success(request):
 
 def subir_comprobante(request, token):
     historial = get_object_or_404(HistorialCompras, token_consulta=token)
-    if historial.forma_de_pago not in ['mixto','transferencia']:
+    if historial.forma_de_pago not in ['mixto','transferencia'] or historial.comprobante:
         return HttpResponse(status=404)
     if request.method == 'POST':
-        print(token)
         form = ComprobanteForm(request.POST, request.FILES)
         if form.is_valid():
             comprobante = form.save(commit=False)
