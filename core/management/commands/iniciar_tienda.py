c903ba4ebfc8b353f1eaebba823c0e8abf64d536
Santiago Aguirre <santiaguirre.lam@gmail.com>
2026-01-15 18:52:46 -0300

    Modo oscuro agregado a la tienda

--- core/management/commands/iniciar_tienda.py
+++ core/management/commands/iniciar_tienda.py
@@ -8,13 +8,17 @@ class Command(BaseCommand):
     help = 'Inicia la tienda configurando los parámetros iniciales'
 
     def handle(self, *args, **kwargs):
-        # Aquí puedes agregar la lógica para iniciar la tienda
         Tienda.objects.get_or_create()
         Proveedor.objects.get_or_create(nombre='Generico')
         gestores_group = Group.objects.create(name='Gestores')
-        staff_user = User.objects.create_user(username='gestor@ts.ar', password='admin123', is_staff=True)
+        staff_user = User.objects.create_user(username='gestor@ts.ar', password='admin123', is_staff=True, email='gestor@ts.ar')
         staff_user.groups.add(gestores_group)
 
-        User.objects.create_superuser(username='superadmin@ts.ar', password='superadmin123', is_superuser=True, is_staff=True)
+        admin = User.objects.create_superuser(username='superadmin@ts.ar', password='superadmin123', is_superuser=True, is_staff=True, email='superadmin@ts.ar')
 
+        staff_user.perfil.email_verificado = True
+        admin.perfil.email_verificado = True
+        staff_user.perfil.save()
+        admin.perfil.save()
+    
         self.stdout.write(self.style.SUCCESS('Tienda iniciada correctamente'))
